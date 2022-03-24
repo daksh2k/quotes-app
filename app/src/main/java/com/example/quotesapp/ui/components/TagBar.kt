b24aef92bf2bb469fc85928d870640cd90f5ddf5
daksh2k <daksh2k@gmail.com>
2022-03-25 01:50:59 +0530

    Refactor: Tagbar to be more reusable
    Signed-off-by: daksh2k <daksh2k@gmail.com>

--- app/src/main/java/com/example/quotesapp/ui/components/TagBar.kt
+++ app/src/main/java/com/example/quotesapp/ui/components/TagBar.kt
@@ -1,12 +1,16 @@
 package com.example.quotesapp.ui.components
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -30,7 +34,58 @@ fun TagBarRow(
     activeTags: List<String>,
     onTagClick: (text: String) -> Unit
 ) {
-    LazyRow(modifier = modifier.padding(horizontal = 8.dp)) {
+    val contentColorForActiveButton =
+        if (!isSystemInDarkTheme()) darkenColor(themeColor) else brightenColor(themeColor)
+    LazyRow(modifier = modifier.padding(8.dp)) {
+        items(activeTags) { tag ->
+            TagButton(
+                border = BorderStroke(1.dp, contentColorForActiveButton),
+                butColors = ButtonDefaults.buttonColors(
+                    backgroundColor = if (isSystemInDarkTheme()) darkenColor(
+                        themeColor,
+                        0.4f
+                    ) else brightenColor(themeColor, 0.4f),
+                    contentColor = contentColorForActiveButton
+                ),
+                text = tag,
+                onTagClick = onTagClick
+            )
+        }
+
+        items(tags) { tag ->
+            TagButton(
+                butColors = ButtonDefaults.buttonColors(
+                    backgroundColor = themeColor.copy(0.2f),
+                    contentColor = themeColor
+                ),
+                text = tag,
+                onTagClick = onTagClick
+            )
+        }
+    }
+}
+
+/**
+ * A simple Lazy row which shows tags.
+ * Uses [TagButton] to show active and inactive tags.
+ * @param tags Inactive tags
+ * @param activeTags Active tags
+ * @param onTagClick What should happen on tag click.
+ */
+@Composable
+fun HeaderTagBarRow(
+    modifier: Modifier = Modifier,
+    themeColor: Color,
+    tags: List<String>,
+    activeTags: List<String>,
+    onTagClick: (text: String) -> Unit
+) {
+    LazyRow(
+        modifier = modifier
+            .padding(8.dp)
+            .background(MaterialTheme.colors.onPrimary)
+            .fillMaxWidth()
+    ) {
         items(activeTags) { tag ->
             TagButton(
                 modifier = Modifier
@@ -39,11 +94,11 @@ fun TagBarRow(
                 butColors = ButtonDefaults.buttonColors(
                     backgroundColor = if (isSystemInDarkTheme()) darkenColor(
                         themeColor,
-                        0.5f
-                    ) else brightenColor(themeColor, 0.5f),
+                        0.3f
+                    ) else brightenColor(themeColor, 0.3f),
                     contentColor = if (!isSystemInDarkTheme()) darkenColor(themeColor) else brightenColor(
                         themeColor
-                    ),
+                    )
                 ),
                 text = tag,
                 onTagClick = onTagClick
@@ -55,17 +110,17 @@ fun TagBarRow(
                 modifier = Modifier
                     .padding(4.dp)
                     .border(1.dp, themeColor, RoundedCornerShape(5)),
-                text = tag,
                 butColors = ButtonDefaults.buttonColors(
                     backgroundColor = themeColor.copy(0.1f),
                     contentColor = themeColor
                 ),
-                enabled = activeTags.size < 3,
+                text = tag,
                 onTagClick = onTagClick
             )
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
